60bf858e0d5637d2a46e7888d076dc0e61844562
trandangkhoa12123 <trandangkhoa12123@gmail.com>
2025-08-28 09:32:11 +0000

    FixGetAllUser

--- src/main/java/com/kblog/api/controllers/UserController.java
+++ src/main/java/com/kblog/api/controllers/UserController.java
@@ -6,13 +6,15 @@ import com.kblog.application.services.UserService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
-@RestController("api/users")
+@RestController
+@RequestMapping("api/users")
 @RequiredArgsConstructor
 public class UserController {
 
